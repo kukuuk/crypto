90ede5726b38dba18bd289608376816b475b7f38
kukuuk <frolofvladislav@gmail.com>
2024-05-07 23:46:35 +0300

    header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,17 @@
 import './styles.css'
+import btcImg from './../../assets/btcImg.jpeg'
 
-const name = 'CRYPTO'
+
 const Header = () => {
     return (
         <header className='header'>
-            {name}
+            <div className='header-text'>
+                <h1>CRYPTO FINANCE</h1>
+                <h2>Check your deposit</h2>
+            </div>
+            <img className='header-img' src={btcImg} alt='btcImg'/>
         </header>
+        
     )
 }
 
